ad80f58b589d0ef783640eb710aa8f645a411bf9
Daniel Persson <daniel@silvertejp.org>
2016-11-09 10:53:34 +0100

    Setup repositories and install use-package

---
+++ init.el
@@ -0,0 +1,13 @@
+; setup repositories
+(require 'package)
+(setq package-check-signature nil)
+(setq package-archives '())
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+
+(package-initialize)
+
+;; install use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
